e1b68095d72cd2f83db0781682446e96b5da1dee
Siddharth <aggarwal.siddharth1@gmail.com>
2020-12-02 20:26:05 +0530

    train.py

--- src/train.py
+++ src/train.py
@@ -1,8 +1,11 @@
+import os
 import pandas as pd 
 from sklearn import preprocessing
+from sklearn import ensemble
+from sklearn import metrics
 
-TRAINING_DATA = None
-FOLD = None
+TRAINING_DATA = os.environ.get("TRAINING_DATA")
+FOLD = int(os.environ.get("FOLD"))
 
 FOLD_MAPPING = {
 0: [1, 2, 3, 4],
@@ -26,6 +29,28 @@ if __name__ == "__main__":
     valid_df = valid_df.drop(['PassengerId','kfold','Survived'])
 
     valid_df = train_df[train_df.columns]
+
+    # Label encoding
+
+    label_encoders = []
+    for c in train_df.columns:
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
+        train_df.loc[:,c] = lbl.transform(train_df[c].values.tolist())
+        valid_df.loc[:,c] = lbl.transform(valid_df[c].values.tolist())
+
+        label_encoders.append((c,lbl))
+
+
+
+    ## Now data is ready to train
+
+    clf = ensemble.RandomForestClassifier(n_jobs=-1, verbose=2)
+    clf.fit(train_df,ytrain)
+
+    preds = clf.predict_proba(valid_df)[:,1]
+
+    print(metrics.roc_auc_score(yvalid,preds))
 
     
 
